ccd70c7b2469dfe86ffb1d47bfe28002d84f97bd
Mi Kyung Han <mhan@calbaptist.edu>
2019-04-15 11:58:56 -0700

    minor fix

--- src/sequentialprinter/SequentialPrintTest.java
+++ src/sequentialprinter/SequentialPrintTest.java
@@ -3,10 +3,6 @@ package sequentialprinter;
 public class SequentialPrintTest {
 
 	public static void main(String[] args) {
-		OddEvenMonitor monitor = new OddEvenMonitor();
-		OddThread t1 = new OddThread(monitor);
-	    EvenThread t2 = new EvenThread(monitor);	
-	    t1.start();
-	    t2.start();
+		//Implement me
 	}
 }
